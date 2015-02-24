3c79beba5707a95f5b7845946ff4779b5ab3805e
c0471944 <c0471944@SB-N208-04.lambton.on.ca>
2015-02-24 13:45:10 -0500

    Added my name in comments of Account class.

--- src/java/model/Account.java
+++ src/java/model/Account.java
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/* Jeff Codling - c0471944 */
+
 package model;
 
 import javax.enterprise.context.ApplicationScoped;
